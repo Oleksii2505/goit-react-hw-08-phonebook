c6a0917d0fbde613973df6e4cd879853ce0349b6
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-05-28 17:50:55 +0300

    Add operations.js and update some code

--- src/components/Redux/store.js
+++ src/components/Redux/store.js
@@ -1,11 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { 
+    persistStore, 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from "./Auth/authSlice";
 import { contactReducer } from "./contactSlice";
 import { filterReducer } from "./filterSlice";
 
+const persistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token']
+}
+
 export const store = configureStore({
     reducer: {
+        auth: persistReducer(persistConfig, authReducer),
         contacts: contactReducer,
-        filter: filterReducer,
-    },   
+        filter: filterReducer       
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),   
 })
+
+export const persistor = persistStore(store);
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
-import { store } from 'components/Redux/store';
+import { store, persistor } from 'components/Redux/store';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
-    <Provider store = {store}>
+   <Provider store = {store}>
+      <PersistGate loading={null} persistor={persistor}>
+       <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <App />  
+       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
